be6b6cde7399be8bb8459ab8a4106272fa9b5570
Charles Sharp <25charlessharp@usd266.com>
2023-01-26 12:03:21 -0600

    Looked at Github

--- FindTagContents.java
+++ FindTagContents.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
 public class FindTagContents {
     public static void main(String[] args) {
-        
+        System.out.println("please type in the HTML you want to search within");
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+
+        System.out.println("Thanks, now enter the search term");
+        String searchTerm = scanner.nextLine();
+
+        int textPos = text.indexOf(searchTerm);
+        int closingTagStartPos = text.indexOf("</", textPos);
+        int closingTagEndPos = text.indexOf(">", closingTagStartPos);
+
+        System.out.print("The text you entered was within a ");
+        System.out.print(text.substring(closingTagStartPos+2, closingTagEndPos));
+        System.out.println(" tag");
+
+        scanner.close();
+
+
+
     }
 }
--- FindWord.java
+++ FindWord.java
@@ -1,5 +1,21 @@
+import javax.sql.rowset.spi.SyncResolver;
+
 public class FindWord {
     public static void main(String[] args) {
-        
+        System.out.println("please type in the sentences");
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+
+        System.out.println("Thanks, now enter the search word");
+        String searchTerm = scanner.nextLine();
+
+        int wordPos = text.indexOf(searchTerm);
+        int endOfSentence = text.indexOf(".", wordPos);
+        String textUpToWord = text.substring(0, wordPos);
+        int startOfSentence = textUpToWord.lastIndexOf(".");
+        System.out.println("the word " + searchTerm + " occurs in sentence");
+        System.out.println(text.subSequence(startOfSentence+2, endOfSentence+1));
+
+        scanner.close();
     }
 }
