aef4908cfc26444dee1af71aadd30017171ae330
Charles Sharp <25charlessharp@usd266.com>
2023-01-27 11:53:58 -0600

    idea

--- FindWord.java
+++ FindWord.java
@@ -1,26 +1,25 @@
 import java.util.Scanner;
 
-import java.util.Scanner;
-
 public class FindWord {
     public static void main(String[] args) {
         System.out.println("please type in the sentences");
         Scanner scanner = new Scanner(System.in);
         String text = scanner.nextLine();
 
-        System.out.println("Thanks, now enter the search word");
+        System.out.println("Thanks, now enter the search letter");
         String searchTerm = scanner.nextLine();
 
         int wordPos = text.indexOf(searchTerm);
-        int endOfSentence = text.indexOf(".", wordPos);
+        int endOfWord = text.indexOf(" ", wordPos);
         String textUpToWord = text.substring(0, wordPos);
-        int startOfSentence = textUpToWord.lastIndexOf(".");
+        int startOfWord = textUpToWord.lastIndexOf(" ");
+        String word = text.substring(startOfWord,endOfWord);
+
         if(wordPos == -1){
             System.out.println("Letter is not in the sentence");
         }
         else{
-            System.out.println("the word " + searchTerm + " occurs in sentence");
-            System.out.println(text.subSequence(startOfSentence+2, endOfSentence+1));
+            System.out.println("The word is " + word);
         }
 
         scanner.close();
