3a4b42adf432b25ea3a16d653627e3b6a1e4386f
Charles Sharp <25charlessharp@usd266.com>
2023-01-27 11:41:43 -0600

    figured out?

--- FindTagContents.java
+++ FindTagContents.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 public class FindTagContents {
     public static void main(String[] args) {
-        System.out.println("please type in the HTML you want to search within");
+        System.out.println("Please type in the HTML you want to search within");
         Scanner scanner = new Scanner(System.in);
         String text = scanner.nextLine();
 
         System.out.println("Thanks, now enter the search term");
         String searchTerm = scanner.nextLine();
 
-        int textPos = text.indexOf(searchTerm);
+        int textPos = text.indexOf("<" + searchTerm + ">");
         int closingTagStartPos = text.indexOf(">", textPos);
         int closingTagEndPos = text.indexOf("</", closingTagStartPos);
 
@@ -18,7 +18,6 @@ public class FindTagContents {
             System.out.println("There is no " + searchTerm + " tags entered in the HTML");
         }
         else{
-
             System.out.print("The text you entered was within a ");
             System.out.print(text.substring(closingTagStartPos+1, closingTagEndPos));
             System.out.println(" tag");
