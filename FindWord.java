5e775d93a23b6de91d6bf9487850a261b96f6501
Charles Sharp <25charlessharp@usd266.com>
2023-01-27 11:59:17 -0600

    fixing things up

--- FindWord.java
+++ FindWord.java
@@ -10,17 +10,17 @@ public class FindWord {
         String searchTerm = scanner.nextLine();
 
         int wordPos = text.indexOf(searchTerm);
-        int endOfWord = text.indexOf(" ", wordPos);
-        String textUpToWord = text.substring(0, wordPos);
-        int startOfWord = textUpToWord.lastIndexOf(" ");
-        String word = text.substring(startOfWord,endOfWord);
-
         if(wordPos == -1){
-            System.out.println("Letter is not in the sentence");
+            System.out.println("That letter is not in the sentence");
         }
         else{
+            int endOfWord = text.indexOf(" ", wordPos);
+            String textUpToWord = text.substring(0, wordPos);
+            int startOfWord = textUpToWord.lastIndexOf(" ");
+            String word = text.substring(startOfWord,endOfWord);
             System.out.println("The word is " + word);
         }
+
 
         scanner.close();
     }
